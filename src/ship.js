37eaa3ba5303491e7565e3ba57021ccb139a3bc1
Faizan <faizanamjad575@gmail.com>
2025-06-12 22:51:12 +0500

    create ship class

---
+++ src/ship.js
@@ -0,0 +1,28 @@
+class Ship {
+    constructor(length) {
+        this.length = length;
+        this.hits = 0;
+        this.sunk = false;
+    }
+
+    hit() {
+        this.hits += 1;
+        if (this.hits >= this.length) {
+            this.sunk = true;
+        }
+    }
+
+    isSunk() {
+        return this.sunk;
+    }
+
+    getLength() {
+        return this.length;
+    }
+
+    getHits() {
+        return this.hits;
+    }
+}
+
+module.exports = Ship;
