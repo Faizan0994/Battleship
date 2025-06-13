67f7c763b05a4e4c41d54b7ddf57502810246085
Faizan <faizanamjad575@gmail.com>
2025-06-13 21:46:28 +0500

    create gameboard class

---
+++ src/gameboard.js
@@ -0,0 +1,95 @@
+class Gameboard {
+    constructor(x, y) {
+        this.rows = x;
+        this.columns = y;
+        this.gameboard = this.generateBoard(this.rows, this.columns);
+        this.shipsCount = 0;
+    }
+
+    // eslint-disable-next-line class-methods-use-this
+    generateBoard(r, c) {
+        const gameboard = Array.from({ length: r }, () =>
+            Array.from({ length: c }, () => null)
+        );
+        return gameboard;
+    }
+
+    getBoard() {
+        return this.gameboard;
+    }
+
+    placeShip(ship, x, y, orientation) {
+        const length = ship.getLength();
+        if (orientation === 'horizontal') {
+            if (
+                !(
+                    this.gameboard[x][y] === undefined ||
+                    this.gameboard[x][y + length - 1] === undefined
+                )
+            ) {
+                for (let i = 0; i < length; i += 1) {
+                    if (
+                        typeof this.gameboard[x][y + i] === 'object' &&
+                        this.gameboard[x][y + i] !== null
+                    ) {
+                        throw new Error('Ships cannot overlap');
+                    }
+                }
+                for (let i = 0; i < length; i += 1) {
+                    this.gameboard[x][y + i] = ship;
+                }
+                this.shipsCount += 1;
+            } else {
+                throw new Error('Ship placement out of bounds');
+            }
+        } else if (orientation === 'vertical') {
+            if (
+                !(
+                    this.gameboard[x][y] === undefined ||
+                    this.gameboard[x + length - 1] === undefined
+                )
+            ) {
+                for (let i = 0; i < length; i += 1) {
+                    if (
+                        typeof this.gameboard[x + i][y] === 'object' &&
+                        this.gameboard[x + i][y] !== null
+                    ) {
+                        throw new Error('Ships cannot overlap');
+                    }
+                }
+                for (let i = 0; i < length; i += 1) {
+                    this.gameboard[x + i][y] = ship;
+                }
+                this.shipsCount += 1;
+            } else {
+                throw new Error('Ship placement out of bounds');
+            }
+        }
+    }
+
+    receiveAttack(x, y) {
+        if (
+            this.gameboard[x] === undefined ||
+            this.gameboard[x][y] === undefined
+        ) {
+            throw new Error('Attack out of bounds');
+        } else if (this.gameboard[x][y] === null) {
+            this.gameboard[x][y] = 'miss';
+        } else if (typeof this.gameboard[x][y] === 'object') {
+            this.gameboard[x][y].hit();
+            if (this.gameboard[x][y].isSunk()) {
+                this.shipsCount -= 1;
+            }
+            this.gameboard[x][y] = 'hit';
+        }
+    }
+
+    allShipsSunk() {
+        if (this.shipsCount === 0) {
+            return true;
+        }
+        return false;
+    }
+}
+
+module.exports = Gameboard;
