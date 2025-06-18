5bb076d61d3073dda5e579173bd1c20ad21facb2
Faizan <faizanamjad575@gmail.com>
2025-06-18 13:29:42 +0500

    include audio resources

--- webpack.config.js
+++ webpack.config.js
@@ -2,42 +2,47 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    index: './src/index.js',
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true,
-  },
+    entry: {
+        index: './src/index.js',
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
 
-  plugins: [
-    new HtmlWebpackPlugin({
-        template: './src/index.html',
-        filename: 'index.html',
-        inject: 'body',
-        scriptLoading: 'defer',
-    }),
-  ],
-
-  devtool: 'source-map',
-
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
-      },
-
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
-      },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            filename: 'index.html',
+            inject: 'body',
+            scriptLoading: 'defer',
+        }),
     ],
-  },
+
+    devtool: 'source-map',
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+
+            {
+                test: /\.(mp3|wav|ogg)$/i,
+                type: 'asset/resource', // emits a separate file and returns the URL
+            },
+        ],
+    },
 };
